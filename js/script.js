fea81e983efbe1a80b7c40dcfe2c961f2dfda0b2
AlanP <aln.10zepeda@gmail.com>
2022-04-19 13:37:03 -0500

    1- Added about us. 2- Added Header dis/scrool

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 const toggle = document.querySelector('.toggle');
+const header = document.querySelector('.header');
 const navbar = document.querySelector('.navbar');
+const sub_menu = document.querySelector('.sub-menu');
+const menu = document.querySelector('.menu');
 const close_div = document.querySelector('.close_div');
+const mediaQuery = window.matchMedia('(min-width: 800px)')
+
+let lastScrollY = window.scrollY;
 
 toggle.addEventListener('click',()=>{
     navbar.style.display ='flex';
@@ -13,4 +19,20 @@ close_div.addEventListener('click',()=>{
     toggle.style.display ='block';
     close_div.style.display ='none';
 });
+
+window.addEventListener("scroll",() => {
+    if (mediaQuery.matches) {
+        if (window.scrollY > 472){
+            header.style.position = 'fixed';
+            sub_menu.style.display ='none';
+            menu.style.background = 'rgb(0, 100, 0,0.9)';
+        }else{
+            sub_menu.style.display ='flex';
+            header.style.position = 'absolute';
+            menu.style.background = 'none';
+        };
+    };
+});
+
+
 
